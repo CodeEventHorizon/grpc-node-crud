32ef5142d38e47d23aebe881341c35373a771357
Nikoloz Muladze <nikoloz.9374@gmail.com>
2023-12-14 17:18:06 +0400

    update: move config

--- config/default.ts
+++ config/default.ts
@@ -1,5 +1,5 @@
 import path from "path";
-require("dotenv").config({ path: path.join(__dirname, "../../.env") });
+require("dotenv").config({ path: path.join(__dirname, "../.env") });
 
 const customConfig: {
   port: number;
